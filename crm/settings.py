5168978526dc977eb1e9c6ab703e46e6314ab335
Nazmus Sakib <20153768+NazShakib@users.noreply.github.com>
2020-07-25 20:31:55 +0600

    Update settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -152,5 +152,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'coder.rafi@gmail.com'
-EMAIL_HOST_PASSWORD = '101381103728sa'
+EMAIL_HOST_USER = 'your email'
+EMAIL_HOST_PASSWORD = 'your email password'
